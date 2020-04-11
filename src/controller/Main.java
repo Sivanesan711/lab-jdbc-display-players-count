04b5bd48a84dcd1cd68aed004fef4781d03d023d
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-04-11 18:38:34 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,21 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.Map.Entry;
+
+import dao.SkillDAO;
+import utility.ConnectionManager;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException, Exception {
+		
+		SkillDAO skldao = new SkillDAO();
+		for(Entry<String,Integer> entry: skldao.skillCount().entrySet())
+		{
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+
+	}
+
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,35 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public TreeMap<String,Integer> skillCount() throws SQLException, Exception
+	{
+		int count = 0;
+		TreeMap<String,Integer> treemap = new TreeMap<String,Integer>();
+		
+		ConnectionManager con = new ConnectionManager();
+		Skill skill = null;
+		
+		String sql = "select name, count(name) from name_skill group by name";
+		Statement st = con.getConnection().createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next())
+		{
+			skill = new Skill(null, sql);
+			skill.setSkillName(rs.getString("name"));
+			count = rs.getInt("count(name)");
+			treemap.put(skill.getSkillName(),count);
+		}
+		return treemap;
+	}
+}
